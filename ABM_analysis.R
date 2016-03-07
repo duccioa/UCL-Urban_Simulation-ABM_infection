f25e383636e769171ba6250df3d9888df6bb1178
duccioa <duccio.aiazzi@gmail.com>
2016-03-07 21:48:57 +0000

    Load data completed

--- ABM_analysis.R
+++ ABM_analysis.R
@@ -1,36 +1,12 @@
 library(ggplot2)
 library(reshape2)
+source('./functions.R')
+scenarios = dir('./data', full.names = F, no..=T)
+all_iter = all.raw.data(scenarios)
+all_Avg = all.Avg.data(scenarios)
 
-
-data_folder = "./data/"
-list_files = list.files(data_folder)
-
-df = read.csv(paste0('./data/', list_files[2]), nrows = 1, skip = 21, sep = ',')
-df = df[-1,]
-read.data = function(List){
-    # define function to remove n characters from a string
-    rm.csv = function(x, n){
-        substr(x, 1, nchar(x) - n)
-    }
-    df = read.csv(paste0('./data/', List[2]), nrows = 1, skip = 21, sep = ',')
-    df = df[-1,]
-    for(i in 1: length(List)){
-        csv = paste0('./data/', List[i])
-        temp = read.csv(csv, nrows = 5000, skip = 21, sep = ',')
-        temp$iter = rm.csv(List[i], 4) 
-        df = rbind(df, temp)
-    }
-    df = df[,c(1,2,6,10,14,18, ncol(df))]
-    names(df) = c('time', 'infected', 'healthy', 'recoveryRate', 'immuneRate', 'population', 'iter')
-    return(df)
-}
-df = read.data(list_files)
-
-df_wide = as.matrix(dcast(df[,c(1,2,3,7)], time ~ iter, value.var = 'infected'))
-df_wide = df_wide[,-1]
-iter_avg = apply(df_wide, MARGIN = 1, mean)
-iter_sd = apply(df_wide, MARGIN = 1, sd)
-iter_lower_confInt = iter_avg - 1.96*(iter_sd/sqrt(ncol(df_wide)-1))
-iter_upper_confInt = iter_avg + 1.96*(iter_sd/sqrt(ncol(df_wide)-1))
-
-
+s = scenarios[2]
+by_scenario = all.Avg.data(s)
+g1 = ggplot(by_scenario, aes(x = time))
+g1 + geom_line(aes(y = iter_avg_infected, colour = 'red')) + 
+    geom_line(aes(y = iter_avg_healthy, colour = 'green'))
---
+++ functions.R
@@ -0,0 +1,76 @@
+################################################################################
+##################### AUXILARY FUNCTIONS #######################################
+################################################################################
+
+## Data manipulation
+# remove .csv from a file name
+rm.csv = function(x, n){
+    substr(x, 1, nchar(x) - n)
+}
+# Load a single csv from a file name
+raw.data = function(scenario){
+    data_folder = paste0('./data/', scenario, '/')
+    file_list = list.files(data_folder)
+    df = read.csv(paste0(data_folder, file_list[2]), nrows = 1, skip = 21, sep = ',')
+    df = df[-1,]
+    for(i in 1: length(file_list)){
+        csv = paste0(data_folder, file_list[i])
+        temp = read.csv(csv, nrows = 5000, skip = 21, sep = ',')
+        temp$iter = rm.csv(file_list[i], 4) 
+        df = rbind(df, temp)
+    }
+    return(df)
+}
+# Remove unused columns
+clean.data = function(raw_df){
+    raw_df = raw_df[,c(1,2,6,10,14,18, ncol(raw_df))]
+    names(raw_df) = c('time', 'infected', 
+                  'healthy', 'recoveryRate', 
+                  'immuneRate', 'population', 'iter')
+    return(raw_df)
+}
+# Calculate average and confidence interval, return a dataframe with avg and ConfInt  
+calc.Avg = function(dtf){
+    df_wide = as.matrix(dcast(dtf[,c(1,2,3,7)], time ~ iter, value.var = 'infected'))
+    df_wide = df_wide[,-1]
+    iter_avg = apply(df_wide, MARGIN = 1, mean)
+    iter_sd = apply(df_wide, MARGIN = 1, sd)
+    iter_lower_confInt = iter_avg - 1.96*(iter_sd/sqrt(ncol(df_wide)-1))
+    iter_upper_confInt = iter_avg + 1.96*(iter_sd/sqrt(ncol(df_wide)-1))
+    temp = cbind(time = seq(1:length(iter_avg)), iter_avg_infected = iter_avg,
+                 iter_avg_healthy = dtf$population[1] - iter_avg,
+                            iter_sd, iter_lower_confInt, 
+                            iter_upper_confInt)
+    temp = data.frame(temp, iter = dtf[,'iter'][1])
+    return(temp)
+}
+
+################################################################################
+##################### MAIN FUNCTIONS ###########################################
+################################################################################
+
+## Read and return a dataframe with raw data
+# For each iteration in each scenario
+all.raw.data = function(scenario_list){
+    df = data.frame()
+    for(i in 1:length(scenario_list)){
+        raw_df0 = raw.data(scenario_list[i]) 
+        temp = clean.data(raw_df0)
+        df = rbind(df, temp)
+    }
+    return(df)
+}
+
+## Return a dataframe of all the scenarios with the average and confidence interval
+all.Avg.data = function(scenario_list){
+    df = data.frame()
+    for(i in 1:length(scenario_list)){
+        raw_df0 = raw.data(scenario_list[i]) 
+        temp = clean.data(raw_df0)
+        temp = calc.Avg(temp)
+        df = rbind(df, temp)
+    }
+    return(df)
+}
+
+
